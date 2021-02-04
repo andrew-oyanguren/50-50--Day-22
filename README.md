3a5e435912118e9e9f747b99e923a0977d78f23e
Andrew <aaoyanguren@outlook.com>
2021-02-03 17:13:42 -0800

    Updated all files
    Completed markup, styling and JavaScript, as well as updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 50-50--Day-22
 Vertical Slider
+
+## Table Of Contents
+* General Information
+* Technologies Used
+
+### General Information
+This project was a really good one. I can see me implementing stuff I learned in this project into future sliders. created a function that took in an argument and used an if statement to check the condition of that argument. Base on if that argument was up or down you would transform translate the div element a percentage of its height, which we got using the web API .clientHeight.
+
+### Technologies Used
+* HTML
+* CSS
+* JavaScript
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,37 @@
 // ===== Variables ===== //
 
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const slidesLength = slideRight.querySelectorAll('div').length;
 
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 // ===== Event Listeners ===== //
 
-
+upBtn.addEventListener('click', () => changeSlide('up'))
+downBtn.addEventListener('click', () => changeSlide('down'))
 
 // ===== Functions ===== //
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+}
